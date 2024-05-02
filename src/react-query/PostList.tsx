f211aa3d89c3172a7e3a91e76e307b99a7b7569d
abhay-dhupar <itsmedhupar@gmail.com>
2024-05-02 11:05:45 +0530

    Infinite Queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -3,9 +3,9 @@ import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
   const pageSize = 10;
-  const [page, setPage] = useState(1);
   const [userId, setUserId] = useState<number>();
-  const { data, error, isLoading } = usePosts({ page, pageSize });
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
@@ -23,24 +23,23 @@ const PostList = () => {
         <option value="3">User 3</option>
       </select>
       <ul className="list-group">
-        {data?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data.pages.map((page) => (
+          <>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </>
         ))}
       </ul>
-      <button
-        disabled={page === 1}
-        onClick={() => setPage(page - 1)}
-        className="btn btn-primary my-3 me-1"
-      >
-        Previous
-      </button>
+
       <button
         className="btn btn-primary my-3"
-        onClick={() => setPage(page + 1)}
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 interface Post {
@@ -9,23 +9,28 @@ interface Post {
   }
 
 interface PostQuery{
-    page: number;
     pageSize: number;
 }
 
 const usePosts = (query: PostQuery) => {
-    return useQuery<Post[], Error>({
+    return useInfiniteQuery<Post[], Error>({
         // /users/1/posts
         queryKey: ['posts', query], // every time query changes, react will re render the page (same as dependencies in useEffect)
-        queryFn: () => axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts",{
+        queryFn: ({pageParam=1}) => axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts",{
             params: {
-                _start: (query.page - 1) * query.pageSize,  // Tells axios from what index to fetch data for different pages
+                _start: (pageParam - 1) * query.pageSize,  // Tells axios from what index to fetch data for different pages
                 _limit: query.pageSize,                      // The number of data to fetch from backend
             }
         })
         .then((res) => res.data),
         staleTime: 1 *60 * 1000, //1 min
-        keepPreviousData: true
+        keepPreviousData: true,
+        getNextPageParam: (lastPage, allPages) => {
+            // if you are on page 1, we should return 2 here (if we are on page 1, all pages has length 1)
+            // if last page has more than 0 posts, then jump to next page, otherwise undefined so we don't go to a new page
+            return lastPage.length>0 ? allPages.length + 1 : undefined;
+            // This will return this to queryFn
+        }
     })
 }
 
