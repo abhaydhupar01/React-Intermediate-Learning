08959fe6040dfab6534071938bf726f287e1d4ec
abhay-dhupar <itsmedhupar@gmail.com>
2024-05-06 13:44:17 +0530

    Prenvented uneccesary re render of Navbar

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -5,7 +5,9 @@ import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter); // here we pass arrow function which select a particular property so that property changes then only our Navbar re-renders, not for other.
+
+  console.log("NavBar Re-render"); // Checking if NavBar re renders
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>
